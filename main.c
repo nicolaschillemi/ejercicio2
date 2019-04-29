b0a5c1346b056f97b7a711c97ee02bcc298c254c
nicolaschillemi <nicolaschillemi@hotmail.com>
2019-04-29 19:24:46 -0300

    Ejercicio 2 de la guia de programacion 1

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    char caracter;
+
+    printf("Ingrese un caracter: ");
+    caracter=getchar();
+
+    printf("\n\nEl caracter ingresado es: %c",caracter);
+
+
+
+
+    return 0;
+}
